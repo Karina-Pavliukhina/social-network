25c761398d826e2e6afb286098f88143f821414a
Karina-Pavliukhina <pavliukhina.k@ukr.net>
2021-09-24 12:34:48 +0300

    create new project Social-network. Create new components: Header, Navbar, Profile

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <img
+        src='http://rockretrospektive.ru/wp-content/uploads/2020/05/cropped-rock-retrospektive-white.png'
+        alt='head'/>
+    </header>
+  )
+}
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+      {/*<img*/}
+      {/*  src='https://cs5.pikabu.ru/images/previews_comm/2015-02_3/14237322009895.jpg'*/}
+      {/*  alt='logo'/>*/}
+      <div>
+        ava + info
+      </div>
+      <div>
+        my post
+      </div>
+      <div>
+        new post
+      </div>
+      <div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  )
+}
+export default Profile;
